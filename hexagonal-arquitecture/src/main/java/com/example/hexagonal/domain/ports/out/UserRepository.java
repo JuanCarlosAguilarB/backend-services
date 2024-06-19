26a39ac3ed4aa218cb0d8be8bf5d81d3bb45f4a4
JuanC <abjuancarlos.12@gmail.com>
2024-06-19 17:52:07 -0500

    feat: create output port for model User(UserRepository) in domain layer.

---
+++ hexagonal-arquitecture/src/main/java/com/example/hexagonal/domain/ports/out/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.hexagonal.domain.ports.out;
+
+
+import com.example.hexagonal.domain.models.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository {
+
+    public void delete(Long id);
+    public User save(User user);
+    public Optional<User> getUserById(UUID id);
+    public Optional<User> updateUser(User user);
+
+}
