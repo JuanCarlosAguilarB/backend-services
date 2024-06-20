e32e50f56825f48855fc832a7c1bf07b180066ab
JuanC <abjuancarlos.12@gmail.com>
2024-06-20 02:13:18 -0500

    feat: disable auto creation of id. The id should be to generate from front.

--- hexagonal-arquitecture/src/main/java/com/example/hexagonal/infrastructure/entities/UserEntity.java
+++ hexagonal-arquitecture/src/main/java/com/example/hexagonal/infrastructure/entities/UserEntity.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 public class UserEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+//    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(unique = true)
