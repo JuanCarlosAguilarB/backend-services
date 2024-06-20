fcd9fc5a75957bff9ec9879873989b75e259de9a
JuanC <abjuancarlos.12@gmail.com>
2024-06-20 02:16:41 -0500

    chore: changes in the user controller, now it is possible to create a user in the db.

--- hexagonal-arquitecture/src/main/java/com/example/hexagonal/infrastructure/controllers/UserController.java
+++ hexagonal-arquitecture/src/main/java/com/example/hexagonal/infrastructure/controllers/UserController.java
@@ -4,10 +4,9 @@ import com.example.hexagonal.appication.UserUseCase;
 import com.example.hexagonal.domain.models.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/users/")
@@ -21,8 +20,8 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
-//        User createdTask = taskService.createTask(User);
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
+        User userCreated = userUseCase.create(user);
+        return new ResponseEntity<>(userCreated, HttpStatus.CREATED);
     }
 
 }
