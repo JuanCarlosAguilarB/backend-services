69677c0e0aedc94251cef449345cb0c15b0f0726
JuanC <abjuancarlos.12@gmail.com>
2024-06-20 03:28:02 -0500

    feat: finish creation of user.

--- hexagonal-arquitecture/src/main/java/com/example/hexagonal/infrastructure/controllers/UserController.java
+++ hexagonal-arquitecture/src/main/java/com/example/hexagonal/infrastructure/controllers/UserController.java
@@ -1,27 +1,34 @@
 package com.example.hexagonal.infrastructure.controllers;
 
 import com.example.hexagonal.appication.UserUseCase;
-import com.example.hexagonal.domain.models.User;
+import com.example.hexagonal.domain.models.UserCreatedRequest;
+import com.example.hexagonal.domain.ports.in.UserPort;
+import com.example.hexagonal.infrastructure.ResponseApi;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1/users/")
 public class UserController {
 
-    private final UserUseCase userUseCase;
+    private final UserPort userUseCase;
 
     public UserController(UserUseCase userUseCase) {
         this.userUseCase = userUseCase;
     }
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        User userCreated = userUseCase.create(user);
-        return new ResponseEntity<>(userCreated, HttpStatus.CREATED);
+    public ResponseEntity<ResponseApi> createUser(@RequestBody UserCreatedRequest user) {
+
+        userUseCase.create(user);
+
+        ResponseApi response = ResponseApi.builder()
+                .message("User Created")
+                .statusCode(HttpStatus.CREATED.value())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
 }
