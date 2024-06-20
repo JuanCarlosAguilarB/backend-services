b7a4a20ffba57d2a1591e508a2ca890b54913973
JuanC <abjuancarlos.12@gmail.com>
2024-06-20 04:19:50 -0500

    feat: create repository for authentication.

---
+++ hexagonal-arquitecture/src/main/java/com/example/hexagonal/domain/ports/out/AuthRepository.java
@@ -0,0 +1,15 @@
+package com.example.hexagonal.domain.ports.out;
+
+
+import com.example.hexagonal.domain.models.Auth;
+import com.example.hexagonal.domain.models.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AuthRepository {
+
+    public void delete(UUID id);
+    public Auth create(User user);
+
+}
