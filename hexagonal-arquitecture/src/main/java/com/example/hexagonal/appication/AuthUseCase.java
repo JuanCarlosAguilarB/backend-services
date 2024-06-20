9896b92b0f3366c45a607f1c5b5902fcccc5d8db
JuanC <abjuancarlos.12@gmail.com>
2024-06-20 14:05:14 -0500

    chore: added functionalities of port un AuthUseCase

--- hexagonal-arquitecture/src/main/java/com/example/hexagonal/appication/AuthUseCase.java
+++ hexagonal-arquitecture/src/main/java/com/example/hexagonal/appication/AuthUseCase.java
@@ -2,6 +2,7 @@ package com.example.hexagonal.appication;
 
 import com.example.hexagonal.domain.models.*;
 import com.example.hexagonal.domain.ports.in.AuthPort;
+import com.example.hexagonal.domain.ports.out.AuthRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -9,6 +10,12 @@ import java.util.UUID;
 @Service
 public class AuthUseCase implements AuthPort {
 
+
+    private final AuthRepository authRepository;
+
+    public AuthUseCase(AuthRepository authRepository) {
+        this.authRepository = authRepository;
+    }
 
     @Override
     public Auth create(AuthRequest authRequest) {
@@ -18,5 +25,17 @@ public class AuthUseCase implements AuthPort {
     @Override
     public void delete(UUID userId) {
 
+    }
+
+    @Override
+    public void validateToken(String token) {
+
+        if (token == null || token.isEmpty()) {
+            throw new IllegalArgumentException("Token is required");
+        }
+
+        if (!authRepository.isValid(token)) {
+            throw new IllegalArgumentException("Invalid token");
+        }
     }
 }
