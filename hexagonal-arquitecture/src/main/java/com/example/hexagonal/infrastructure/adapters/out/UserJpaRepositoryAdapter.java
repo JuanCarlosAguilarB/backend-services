605e7c8df1426f904a70737de5294ca9fa447618
JuanC <abjuancarlos.12@gmail.com>
2024-06-20 02:15:33 -0500

    chore: added mappers and functionality to save user in the db.

--- hexagonal-arquitecture/src/main/java/com/example/hexagonal/infrastructure/adapters/out/UserJpaRepositoryAdapter.java
+++ hexagonal-arquitecture/src/main/java/com/example/hexagonal/infrastructure/adapters/out/UserJpaRepositoryAdapter.java
@@ -2,6 +2,9 @@ package com.example.hexagonal.infrastructure.adapters.out;
 
 import com.example.hexagonal.domain.models.User;
 import com.example.hexagonal.domain.ports.out.UserRepository;
+import com.example.hexagonal.infrastructure.entities.UserEntity;
+import com.example.hexagonal.infrastructure.ports.out.UserJpaRepository;
+import com.example.hexagonal.mapper.UserMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,14 +12,27 @@ import java.util.UUID;
 
 @Repository
 public class UserJpaRepositoryAdapter implements UserRepository {
+
+    private final UserJpaRepository userJpaRepository;
+    private final UserMapper userMapper;
+
+    public UserJpaRepositoryAdapter(UserJpaRepository userJpaRepository, UserMapper userMapper) {
+        this.userJpaRepository = userJpaRepository;
+        this.userMapper = userMapper;
+    }
+
     @Override
-    public void delete(Long id) {
+    public void delete(UUID id) {
 
     }
 
     @Override
     public User save(User user) {
-        return null;
+
+        UserEntity userEntity = userMapper.toEntity(user);
+        UserEntity userSaved = userJpaRepository.save(userEntity);
+
+        return userMapper.toDomain(userSaved);
     }
 
     @Override
